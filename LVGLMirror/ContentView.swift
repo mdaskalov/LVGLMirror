b1406395e2af1cb9aff8ce6590a1aec63eeca07b
Milko Daskalov <mdaskalov@outlook.com>
2026-01-01 13:19:59 +0100

    Initial Commit

---
+++ LVGLMirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LVGLMirror
+//
+//  Created by Milko Daskalov on 01.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LVGLMirror/LVGLMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVGLMirrorApp.swift
+//  LVGLMirror
+//
+//  Created by Milko Daskalov on 01.01.26.
+//
+
+import SwiftUI
+
+@main
+struct LVGLMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
